7e3265f53e699735346a37dca181719ae0f43b06
Nikita Maiboroda <30575825+NM-tl@users.noreply.github.com>
2022-06-08 21:21:08 +0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -5,11 +5,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const total = document.querySelector('.total')
 
-
     const renderDesignList = design.map(renderDesign)
     const renderServicesList = services.map(renderService)
     const renderCountriesList = countries.map(renderCountry)
-    // const percent = countries.map(getPercent)
+    const percent = countries.map(getPercent)
 
     if (listServices && Array.isArray(renderServicesList)) {
         listServices.innerHTML = renderServicesList.join('');
@@ -23,62 +22,41 @@ document.addEventListener('DOMContentLoaded', () => {
         listCountries.innerHTML = renderCountriesList.join('');
     }
 
-
-    // selectDesign();
-    // selectCheckbox();
-
-    // let designVal = selectDesign();
-    //
-    // if(designVal === undefined) {
-    //     designVal = 0;
-    // }
-    //
-    // console.log(`DesVal: ${designVal}`);
-    //
-    // let serviceVal = selectCheckbox();
-    //
-    // if(serviceVal === undefined) {
-    //     serviceVal = 0;
-    // }
-    //
-    // console.log(`ServVal: ${serviceVal}`);
-    //
-    // let sum = designVal + serviceVal;
-    //
-    // console.log(`Total: ${sum}`);
-
-    // calc();
-
-
-    // const serviceCheck = document.querySelectorAll('.service')
-    // const designCheck = document.querySelectorAll('.design')
-
-    // let designValue = getDesignValue(designCheck);
-    // let cbVal = getServiceValue(serviceCheck);
-
-
-    const percentTotal = document.querySelectorAll('.country-price')
     const inputs = document.querySelectorAll('input')
-
+    const resultList = document.querySelector('.result')
 
     function calc() {
         let totalPrice = 0;
 
-        for(const input of inputs) {
+        for (const input of inputs) {
             input.addEventListener('input', function() {
-                if(input.checked){
-                    totalPrice += parseInt(input.value);
-                    console.log(`TotalPrice: ${totalPrice}`);
-                    total.innerHTML = `Total Price: ${totalPrice}$`;
-                    percentTotal.innerHTML = totalPrice / 100 * 5
+                if (input.checked) {
 
+                    totalPrice += parseInt(input.value);
+                    total.innerHTML = `Total Price: ${totalPrice}$`;
+
+                    const result = percent.map((num) => parseInt(totalPrice) / 100 * num + parseInt(totalPrice));
+                    const renderResultList = result.map(renderResult)
+
+                    if(resultList && Array.isArray(renderResultList)) {
+                        resultList.innerHTML = renderResultList.join('');
+                    }
+                } else if (!input.checked) {
+                    totalPrice -= parseInt(input.value);
+                    total.innerHTML = `Total Price: ${totalPrice}$`;
+
+                    const result = percent.map((num) => parseInt(totalPrice) / 100 * num + parseInt(totalPrice));
+                    const renderResultList = result.map(renderResult)
+
+                    if(resultList && Array.isArray(renderResultList)) {
+                        resultList.innerHTML = renderResultList.join('');
+                    }
                 }
+
             })
         }
     }
-
     calc();
-
 })
 
 const design = [
@@ -100,7 +78,12 @@ const countries = [
     {name: 'Japan', percent: 20, img: 'jap'}
 ];
 
+
 const toLowerCase = (str) => typeof srt === 'string' ? str.toLowerCase() : str;
+
+function renderResult(item) {
+    return `<div class="result-item">${item}$</div>`
+}
 
 function renderDesign(item) {
     return `<span class="ds">
@@ -120,119 +103,10 @@ function renderCountry(items) {
     return `<div class="countries-container">
                 <div class="countries-flag" style="background-image: url('assets/images/${items.img}.svg')"></div>
                 <div class="item-name">${items.name}</div>
-                <div class="country-price">${items.percent}%</div>
+                <!--<div class="country-price">${items.percent}%</div>-->
 </div>`
 }
 
 function getPercent(item) {
-    let countryPercent = item.percent;
-    return console.log(countryPercent);
+    return item.percent;
 }
-
-
-
-// function getDesignValue(designCheck) {
-//     designCheck.forEach((checkbox) => {
-//         checkbox.addEventListener('change', (event) => {
-//
-//             if (event.target.checked) {
-//                 let designValue = event.target.value;
-//                 return designValue;
-//             }
-//         })
-//     })
-// }
-//
-// function getServiceValue(allCheckbox) {
-//     allCheckbox.forEach((checkbox) => {
-//         checkbox.addEventListener('change', (event) => {
-//
-//             if (event.target.checked) {
-//                 let serviceVal = event.target.value;
-//                 return console.log(serviceVal);
-//             }
-//         })
-//     })
-// }
-
-// переписать селект на инпут чек-бокс
-
-
-
-
-
-
-// function selectDesign() {
-//     const select = document.getElementById('design');
-//
-//     select.addEventListener('change', function() {
-//         let getVal = this.value;
-//         return console.log(getVal);
-//     })
-// }
-
-// function selectCheckbox(serviceVal) {
-//     const allCheckbox = document.querySelectorAll('.service')
-//
-//     allCheckbox.forEach((checkbox) => {
-//         checkbox.addEventListener('change', (event) => {
-//
-//             if (event.target.checked) {
-//                 let serviceVal = event.target.value;
-//                 return console.log(serviceVal);
-//             }
-//         })
-//     })
-// }
-
-// function calc(selectVal, serviceVal) {
-//     const allCheckbox = document.querySelectorAll('.service')
-//     const select = document.getElementById('design');
-//
-//     select.addEventListener('change', function() {
-//         selectVal = this.value;
-//         return selectVal;
-//     })
-//
-//     console.log(selectVal);
-//
-//     allCheckbox.forEach((checkbox) => {
-//         checkbox.addEventListener('change', (event) => {
-//
-//             if (event.target.checked) {
-//                 let serviceVal = event.target.value;
-//                 return console.log(serviceVal);
-//             }
-//         })
-//     })
-//
-//
-// }
-
-
-
-// function calc() {
-//     const designPrice = services.map((i) => i.price);
-//     const servicesPrice = design.map((i) => i.price);
-//     const countriesPercent = countries.map((i) => i.percent);
-//
-//     // return console.log(...designPrice)
-//
-//     function arraySum(arr) {
-//         let sum = 0;
-//         for(let i = 0; i < arr.length; i++){
-//             sum += arr[i];
-//         }
-//         return sum;
-//     }
-//
-//     const total = () => (arraySum(designPrice) + arraySum(servicesPrice)) / 100 * arraySum(countriesPercent);
-//
-//
-//     function renderTotal(totalPrice) {
-//         return document.querySelector('.total').innerHTML = total(totalPrice);
-//     }
-//
-//     renderTotal()
-// }
-
